ee08b6c2b5e8814bb1d8acb1681921bb9fd2a5cd
BroccoliRabe <evaldas.liaugaudas@mcc.edu>
2017-04-19 15:05:11 -0400

    Add project files.

---
+++ MstrWrksStudio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MstrWrksStudio.Startup))]
+namespace MstrWrksStudio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
